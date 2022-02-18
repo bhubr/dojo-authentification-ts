9f45de853f3455a6c1c4fc7e43525a6a9d618d75
Benoît Hubert <benoithubert@gmail.com>
2022-02-18 17:33:39 +0100

    Add JWT check middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+
+import jwt from 'jsonwebtoken';
+import { jwtSecret } from './config';
 
 import { port } from './config';
 import login from './login';
+import { IUserOut } from './types';
 
 const app=  express()
 app.use(express.json())
@@ -14,7 +18,46 @@ app.use(express.json())
 
 
 //question hervé : 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', checkToken, (req: any, res) => res.send(`Hello ${req.user.username}`));
+
+// type RequestWithUser = Request & { user: IUserOut }
+
+// régler pb de type !
+async function checkToken (req: any, res: Response, next: NextFunction) {
+  //recevoir le token
+
+  const authorization = req.headers.authorization;  // Bearer JWT
+  //check le token si undefined
+  if(authorization===undefined){
+    //unauthorized 401
+    return res.status(401).send({
+      error: "no token found"
+    })
+  }
+
+  // Séparer "Bearer" et le token
+  //on split avec espace
+  const [, token] = authorization.split(' ') // ['Bearer', JWT]
+  console.log('token', token)
+  //decoder et vérifier : la signature du token si correcte (if token is generated with the server's jwt secret)
+  //voir github jsonwebtoken
+  
+  try {
+    req.user = jwt.verify(token, jwtSecret)
+    next()
+    // si c'est pas bon : retourner "invalid token"
+  } catch (error) {
+    console.error(error)
+    return res.status(401).send({
+      error: "invalid token"
+    })
+  }
+  // si c'est bon => next()
+  // si c'est pas bon : retourner "invalid token" ou 'expired token'
+
+  
+}
+
 
 app.post('/login', async (req, res) => {
   try {
@@ -37,5 +80,8 @@ app.post('/login', async (req, res) => {
     }
   }
 })
+
+
+
 //eviter les ports en dessous de 1024
 app.listen(port, ()=>console.log('server is running'))
