d498b735cf4875c7aeda83fe6d841fca6af2e2af
Benoît Hubert <benoithubert@gmail.com>
2022-02-18 16:06:11 +0100

    Complete login function and test it with Express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import express from 'express'
 
-
+const app=  express()
+app.use(express.json())
+//lire le fichier env
+dotenv.config()
 
 // Fonction `login`
 // en entrée : { "username": "...", "password": "..."}
@@ -24,7 +30,7 @@ type IUserDB = IUserIn & IUserOut
 // prisma.user.findUnique({ where: { username } })
 
 const usersDb: IUserDB[] = [
-  { _id: 'abcd000', username: 'johndoe', email: 'johndoe', password: '$argon2i$v=19$m=4096,t=3,p=1$KaST0EiD7vfRwyTCTo7VzA$Q3kkOC+HpbbnhF5fGOuROkF6hrEh2/Bsxr5JFhaf9Dk' }
+  { _id: 'abcd000', username: 'johndoe', email: 'johndoe@toto.net', password: '$argon2i$v=19$m=4096,t=3,p=1$KaST0EiD7vfRwyTCTo7VzA$Q3kkOC+HpbbnhF5fGOuROkF6hrEh2/Bsxr5JFhaf9Dk' }
 ]
 
 async function findUserByUsername(username: string) {
@@ -32,38 +38,57 @@ async function findUserByUsername(username: string) {
   return user
 }
 
-async function login({ username, password }:IUserIn): Promise<{ user: IUserOut, jwt: string }>{
+async function login({ username, password }:IUserIn): Promise<{ user: IUserOut, token: string }>{
+    // Récupérer l'user depuis la BDD
+    // IMPORTANT : ce code doit être adapté pour votre "moteur" de BDD ou ORM (Prisma, TypeORM, etc.)
     const user = await findUserByUsername(username);
 
     // user : { "password": "<chiffré>" }
     
     if (!user) {
-        throw new Error("User not found");
+        throw new Error("Invalid credentials");
     }
 
     const isValid: boolean = await argon2.verify(user.password, password);
 
     if (!isValid) {
-        throw new Error("User not found");
+        throw new Error("Invalid credentials");
     }
 
-    const jwt = jwt.sign({
-        exp: Math.floor(Date.now()/1000) + (60*60),
-        data:  { ...user, user._id}
-    })
+    // const { password: x, ...rest } = user
+    const { _id, email } = user
+    const token = jwt.sign({
+      _id, email, username,
+        // data:  { _id: user._id, username, email: user.email }
+    }, process.env.JWT_SECRET || 'secret', { expiresIn: '1h' });
 
-    return { user, jwt }
+    return { user, token }
     
 }
+
 //question hervé : 
+app.get('/', (req, res) => res.send('Hello'));
 
 app.post('/login', async (req, res) => {
   try {
-    const { user, jwt } = await login(req.body)
-    res.send({ user })
-  } catch (err) {
-      if (err.message === 'User not found') {
-        return res.sendStatus(404)
+    const { user, token } = await login(req.body)
+    res.send({ user, token })
+  } 
+  //previlegier unknown : no garanti (sur le type de err)
+  catch (err: unknown) {
+    console.error(err)
+    if (err instanceof Error) {
+      if (err.message === 'Invalid credentials') {
+        return res.sendStatus(401)
+      } else {
+        return  res.status(500).send({
+          error: err.message
+        })
       }
+    } else {
+     return res.sendStatus(500)
+    }
   }
 })
+//eviter les ports en dessous de 1024
+app.listen(process.env.PORT || 5000 ,()=>console.log('server is running'))
